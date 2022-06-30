c993ad1d6011bfe72652481079f0468bff407ff4
jnsgdm <JonasDmoreira@hotmail.com>
2022-06-30 17:19:10 -0300

    commit

---
+++ js/dogBreedList.js
@@ -0,0 +1,33 @@
+async function dogBreeds(){
+    const res = await fetch('https://dog.ceo/api/breeds/list/all');
+    const JSONres = await res.json();
+    const data = JSONres.message;
+    //console.log(data);
+    
+    const breedKeys = Object.keys(data);
+    const arr = [];
+
+    //format the dogs breeds names and append in an array
+    breedKeys.forEach( (e) =>{
+        if(data[e].length > 0 ){
+            for(var i = 0; i < data[e].length; i++){
+                const firstBreedName = e;
+                const lastBreedName = data[e];
+                const fullBreedName = `${firstBreedName} ${lastBreedName[i]}`;
+                arr.push(fullBreedName);
+            }
+        }else{
+            arr.push(e);
+        }
+    });
+
+    //generate the dogs breeds selection list 
+    arr.forEach( (eachDog) =>{ 
+        const createOptions = document.createElement('option');
+        createOptions.setAttribute('id',`${eachDog}`);
+        createOptions.setAttribute('value', `${eachDog}`);
+        document.getElementById('dogList').appendChild(createOptions).innerHTML = eachDog;
+    });
+}
+
+dogBreeds()
