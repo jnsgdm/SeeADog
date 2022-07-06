2118f3c6cfc833b2f080f3d2e705096881457e45
jnsgdm <JonasDmoreira@hotmail.com>
2022-07-06 20:26:28 -0300

    favicon

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 3000
 
 app.use(express.static(__dirname + '/css'));
 app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + "/index.html")
